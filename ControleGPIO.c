b350937bb270fb91baf1bac1733e2856b97ebd38
Talles Gomes <tallespk@gmail.com>
2025-01-17 10:34:58 -0300

    Esquema inicial da tarefa

---
+++ ControleGPIO.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+
+
+const uint Buzzer = 10;
+const uint led_G = 11;
+const uint led_B = 12;
+const uint led_R = 13;
+#define Tempo_led = 500;
+
+//as funçoes de  acender e apagar os leds individualmente
+
+
+
+//função de leitura do teclado
+
+
+
+//função de acionamento do buzzer, um pra erro e outro para acerto
+
+
+
+
+
+
+
+int main(){
+    //inicialização das portas com os pinos e o buzzer
+
+
+
+    
+    //inicialização do teclado
+
+
+    
+
+while(true){
+ // definir os valores para comparação
+
+
+
+ // Ler o teclado e fazer os comparativos
+
+
+ // acender o led de acordo com o valor do teclado
+
+
+}
+
+
+}
